1f79895a84c15885daf6d29a4e831d7774a3aefe
BCPTe <56164897+BCPTe@users.noreply.github.com>
2020-04-08 17:02:20 +0200

    Add files via upload

--- OOP_LAB_University/src/ExampleApp.java
+++ OOP_LAB_University/src/ExampleApp.java
@@ -1,4 +1,5 @@
 import university.*;
+import university.UniversityExt;
 
 public class ExampleApp {
 
@@ -6,7 +7,7 @@ public class ExampleApp {
 		
 		String universityName = "Politecnico di Torino";
 		
-		University poli = new University(universityName);
+		UniversityExt poli = new UniversityExt(universityName);
 		
 		poli.setRector("Guido", "Saracco");
 		
@@ -14,24 +15,46 @@ public class ExampleApp {
 		
 		int s1 = poli.enroll("Mario","Rossi");
 		int s2 = poli.enroll("Giuseppe","Verdi");
+		int s3 = poli.enroll("Take", "Away");
+		int s4 = poli.enroll("Giorgio", "Africano");
 		
-		System.out.println("Enrolled students " + s1 + ", " + s2); // 10000, 10001
+		System.out.println("Enrolled students " + s1 + ", " + s2 + ", " + s3 + ", " + s4); // 10000, 10001, 10002, 10003
 		System.out.println("s1 = " + poli.student(s1)); // 10000 Mario Rossi
 		
 		int macro = poli.activate("Macro Economics", "Paul Krugman");
 		int oop = poli.activate("Object Oriented Programming", "James Gosling");
 		
 		System.out.println("Activated courses " + macro + " and " + oop); // 10 and 11
-		poli.register(s1, macro);
+		//poli.register(s1, macro);
 		poli.register(s2, macro);
 		poli.register(s2, oop);
+		poli.register(s3, macro);
+		poli.register(s3, oop);
+		poli.register(s4, oop);
+		poli.register(s4, macro);
 		
-		System.out.println(poli.listAttendees(macro));
+		System.out.println("List Attendes " + macro + ":\n" + poli.listAttendees(macro));
 		// 10000 Mario Rossi
 		// 10001 Giuseppe Verdi
 		
-		System.out.println(poli.studyPlan(s2));
+		System.out.println("Study Plan s2:\n" + poli.studyPlan(s2));
 		// 10,Macro Economics,Paul Krugman
 		// 11,Object Oriented Programming,Marco Torchiano
+		
+		poli.exam(s2, oop, 68);
+		poli.exam(s2, macro, 89);
+		poli.exam(s1, oop, 55);
+		poli.exam(s3, oop, 57);
+		poli.exam(s4, oop, 69);
+		poli.exam(s4, macro, 98);
+		System.out.println(poli.studentAvg(s1));
+		System.out.println(poli.studentAvg(s2));
+		System.out.println(poli.studentAvg(s3));
+		System.out.println(poli.studentAvg(s4));
+		
+		System.out.println(poli.courseAvg(macro));
+		System.out.println(poli.courseAvg(oop));
+		
+		System.out.println("\nTop Three:\n" + poli.topThreeStudents());
 	}
 }
